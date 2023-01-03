431dac90cae51c5cb3a8d9545d6e4a35a99379a5
hugo pereira <hugo.pe.po@hotmail.com>
2023-01-03 10:45:34 -0300

    Criação do MCR ProjetoSprint

---
+++ app/Http/Controllers/ProjetoSprintController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjetoSprint;
+use Illuminate\Http\Request;
+
+class ProjetoSprintController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjetoSprint  $projetoSprint
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjetoSprint $projetoSprint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjetoSprint  $projetoSprint
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjetoSprint $projetoSprint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProjetoSprint  $projetoSprint
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjetoSprint $projetoSprint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjetoSprint  $projetoSprint
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjetoSprint $projetoSprint)
+    {
+        //
+    }
+}

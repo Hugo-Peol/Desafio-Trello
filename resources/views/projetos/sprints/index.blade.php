5222e4e6be626407867ab4cd71cdddab59c6dc56
hugo pereira <hugo.pe.po@hotmail.com>
2023-01-10 10:09:28 -0300

    Refatorando código

--- resources/views/projetos/sprints/index.blade.php
+++ resources/views/projetos/sprints/index.blade.php
@@ -1,77 +1,25 @@
-<x-style title="Sprints" >
-              {{-- lista select --}}  
-<div class="mt-2">
-  <label for="" class="form-label">Selecione a sprint:</label>
-  <select class="form-select form-select-lg" name="tipo_projeto" id="TipoProjeto">
-      <option selected>1</option>
-      <option value="">2</option>
-  </select>
+<x-style title="Sprint Nº {{$sprint->id}}" >
+
+    {{-- lista select --}}
+
+@isset($mensagemSucesso)
+<div class="alert alert-success">
+    {{ $mensagemSucesso }}
 </div>
-<br>
-</div>
+@endisset
 
 
-<div class="container mt-2">
-    <a href="{{ route('projetos.sprints.create', $projeto->id) }}">
-        <button type="button" class="btn btn-primary">Criar nova sprint</button>
+<div class="btn-group" role="group" aria-label="Basic example">
+    <a href="{{ route('projetos.sprints.create', $projeto->id) }}"  class="btn btn-success ">
+    Ver sprint
     </a>
 
 
-</div>
-{{--(FIM) cadastrar sprint --}}
 
-{{-- grid dos status das tarefas --}}
 
-<div class="m-5 text-center bg-dark h-100">
-  <div class="row align-items-start p-1" style="height: 65vh">
-    <div class="col h-100">
-      <x-card-column>
 
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
 
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
 
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-    <div class="col h-100">
-      <x-card-column>
-        
-      </x-card-column>
-    </div>
-  </div>
-</div>
 
-{{--(FIM) grid dos status das tarefas --}}
 
 </x-style>
